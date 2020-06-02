c5cf0617e5b4ccf191f55cb63d2fc197db07f03f
brugobi <brgobbi@hotmail.com>
2020-06-02 11:24:19 -0400

    add my each

--- enumerable.rb
+++ enumerable.rb
@@ -1,3 +1,15 @@
-def method_name
+module Enumerable
+    def my_each
+      
+      (self.size).times do |n|
+        
+      end
+       
+    end
     
 end
+
+
+arr = [1,2,3,4,5,6]
+
+puts arr.my_each {|n| n * 2}
