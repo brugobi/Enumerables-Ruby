61a27a3a8204b97e38dc82cd802f082da697a319
brugobi <brgobbi@hotmail.com>
2020-06-02 11:59:22 -0400

    add my_each_with_index

--- enumerable.rb
+++ enumerable.rb
@@ -3,9 +3,15 @@ module Enumerable
     return to_enum unless block_given?
     self.size.times { |n| yield (self[n]) }
   end
+
+  def my_each_with_index
+    return to_enum unless block_given?
+    self.size.times {|n| yield self[n], n}
+  end
+
 end
 
 
 arr = [1, 2, 3, 4, 5, 6]
 
-arr.my_each { |n| puts n * 2 }
+arr.my_each_with_index { |n, i| puts "this is my #{n} and this is my index #{i}" }
