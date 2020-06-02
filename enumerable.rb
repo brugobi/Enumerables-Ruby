f244933902bca0fcf808c8ec4d443f020318347a
asiimwedismas <asiimwedismas@gmail.com>
2020-06-02 22:20:41 +0300

    Added the my_map method

--- enumerable.rb
+++ enumerable.rb
@@ -1,50 +1,62 @@
 module Enumerable
   def my_each
     return to_enum unless block_given?
-    self.size.times { |n| yield (self[n]) }
+
+    size.times { |n| yield (self[n]) }
   end
 
   def my_each_with_index
     return to_enum unless block_given?
-    self.size.times { |n| yield self[n], n }
+
+    size.times { |n| yield self[n], n }
   end
 
   def my_select
     return to_enum unless block_given?
+
     result_array = []
-    self.my_each { |n| result_array << n if yield(n) }
+    my_each { |n| result_array << n if yield(n) }
     result_array
   end
 
-
   def my_all
     return true unless block_given?
-    self.my_each { |n| return false unless yield(n) }
+
+    my_each { |n| return false unless yield(n) }
     true
   end
 
   def my_any
     return true unless block_given?
-    self.my_each { |n| return true if yield(n) }
+
+    my_each { |n| return true if yield(n) }
     false
   end
 
   def my_none
     return true unless block_given?
-    self.my_each { |i| return false if yield(i) == true }
+
+    my_each { |i| return false if yield(i) == true }
     true
   end
 
   def my_count
     counter = 0
-    self.my_each {|i| counter +=1 if yield(i)}
+    my_each { |i| counter += 1 if yield(i) }
     counter
-  end  
+  end
+
+  def my_map
+    return to_enum unless block_given?
+
+    map_array = []
+    my_each { |n| map_array << yield(n) }
+    map_array
+  end
 end
 
-
-arr = [1,1]
+arr = [1, 1, 45, 23, 56]
 
 # arr.my_each_with_index { |n, i| puts "this is my #{n} and this is my index #{i}" }
 
-puts arr.my_count{|n| n.even?}
+puts arr.my_map(&:even?)
