c459a8fc3e337dbd49a7f36b5a6e8f7e875dffa4
asiimwedismas <asiimwedismas@gmail.com>
2020-06-02 19:01:10 +0300

    Created the my_select method

--- enumerable.rb
+++ enumerable.rb
@@ -6,7 +6,14 @@ module Enumerable
 
   def my_each_with_index
     return to_enum unless block_given?
-    self.size.times {|n| yield self[n], n}
+    self.size.times { |n| yield self[n], n }
+  end
+
+  def my_select
+    return to_enum unless block_given?
+    result_array = []
+    self.my_each { |n| result_array << n if yield(n) }
+    result_array
   end
 
 end
@@ -14,4 +21,6 @@ end
 
 arr = [1, 2, 3, 4, 5, 6]
 
-arr.my_each_with_index { |n, i| puts "this is my #{n} and this is my index #{i}" }
+# arr.my_each_with_index { |n, i| puts "this is my #{n} and this is my index #{i}" }
+
+puts arr.my_select{|n| n.even?}
