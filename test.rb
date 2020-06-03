26e3d0e5225e83d62516c40e97e582c14331239f
asiimwedismas <asiimwedismas@gmail.com>
2020-06-03 20:25:01 +0300

    Added a few tests for my_inject method

--- test.rb
+++ test.rb
@@ -7,3 +7,18 @@ end
 arr = [2, 4, 5]
 
 puts multiply_els(arr)
+
+# arr = [5, 6, 7, 8, 9, 10]
+
+# puts arr.my_inject(:+) #=> 45
+
+# puts arr.my_inject { |sum, n| sum + n } #=> 45
+
+# puts arr.my_inject(1, :*) #=> 151200
+
+# puts arr.my_inject(1) { |product, n| product * n } #=> 151200
+# # find the longest word
+# longest = %w[cat sheep bear].my_inject do |memo, word|
+#   memo.length > word.length ? memo : word
+# end
+# puts longest #=> "sheep"
