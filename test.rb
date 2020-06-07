adb7b0dfc9dff1847d5f520bbe09904c8ec8277d
asiimwedismas <asiimwedismas@gmail.com>
2020-06-07 09:39:05 +0300

    Moved multiply_els to enumerable.rb

--- test.rb
+++ test.rb
@@ -1,13 +1,5 @@
 load 'enumerable.rb'
 
-def multiply_els(arr)
-  arr.my_inject { |sum, n| sum * n }
-end
-
-arr = [2, 4, 5]
-
-puts multiply_els(arr)
-
 # arr = [5, 6, 7, 8, 9, 10]
 
 # puts arr.my_inject(:+) #=> 45
