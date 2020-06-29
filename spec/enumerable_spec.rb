3d90c8b7a2ef57a3cd867e95b4d470c1f7c5fbdb
Adrian <adfpizarro@gmail.com>
2020-06-29 13:51:49 -0600

    Added the methods to test my_each with inde

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -2,10 +2,32 @@ require_relative '../enumerable.rb'
 
 describe Enumerable do
     let(:array) { [1, 2, 3, 4, 5, 6, 7, 8, 9] }
-    describe '#My_each' do
-        it "checking my_each method" do
-            expect(array.my_each { |number| number*2 }).to eql(array.each { |number| number*2 })
-        end 
-    end       
-end
 
+    describe '#My_each' do
+        it "checking my_each with block" do
+            expect(array.my_each { |number| number*2 }).to eql(array.each { |number| number*2 })
+        end
+    end
+
+    describe '#My_each_with_index' do
+        it "checking my_each_with_index with index block given" do
+            expect(array.my_each_with_index  { |num, ind| puts "item #{num} and #{ind}" }).to eql(array.each_with_index  { |num, ind| puts "item #{num} and #{ind}" })
+        end
+    end
+
+    describe '#My_each_with_index' do
+      block = proc { |num, ind| puts "item #{num} and #{ind}" }
+        it "checking my_each_with_index with index block given" do
+            expect(array.my_each_with_index(&block)  ).to eql(array.each_with_index(&block))
+        end
+    end
+
+    describe '#My_select' do
+      block = proc { |num, ind| puts "item #{num} and #{ind}" }
+        it "checking my_select with index block given" do
+            expect(array.my_select { |n| n.even? }  ).to eql(array.my_select { |n| n.even? })
+        end
+    end
+
+
+end
