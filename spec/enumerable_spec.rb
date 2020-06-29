b9d83bdd73a55dd3bd1b7a2361669779f6a1b0c2
brugobi <brgobbi@hotmail.com>
2020-06-29 16:34:16 -0400

    add testing for my_all, my_any, my_count and my_none

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -2,6 +2,11 @@ require_relative '../enumerable.rb'
 
 describe Enumerable do
     let(:array) { [1, 2, 3, 4, 5, 6, 7, 8, 9] }
+    let(:words) {%w[cat, dog, bird]}
+    let(:array_empty) {[]}
+    let(:array_mixed) {[nil, true, 99]}
+    let(:array_numeric) {[1, 2i, 3.14]}
+    
 
     describe '#My_each' do
         it "checking my_each with block" do
@@ -13,10 +18,8 @@ describe Enumerable do
         it "checking my_each_with_index with index block given" do
             expect(array.my_each_with_index  { |num, ind| puts "item #{num} and #{ind}" }).to eql(array.each_with_index  { |num, ind| puts "item #{num} and #{ind}" })
         end
-    end
 
-    describe '#My_each_with_index' do
-      block = proc { |num, ind| puts "item #{num} and #{ind}" }
+        block = proc { |num, ind| puts "item #{num} and #{ind}" }
         it "checking my_each_with_index with index block given" do
             expect(array.my_each_with_index(&block)  ).to eql(array.each_with_index(&block))
         end
@@ -26,6 +29,94 @@ describe Enumerable do
       block = proc { |num, ind| puts "item #{num} and #{ind}" }
         it "checking my_select with index block given" do
             expect(array.my_select { |n| n.even? }  ).to eql(array.my_select { |n| n.even? })
+        end
+    end
+
+    describe '#My_all' do
+        it "checking my_all with array numbers" do
+            expect(array.my_all? { |n| n > 5 }  ).to eql(array.all? { |n| n > 5 } )
+        end
+
+        it "checking my_all with array words" do
+            expect(words.my_all? { |n| n.length > 2 }).to eql(words.all? { |n| n.length > 2 })
+        end
+
+        it "checking my_all with array empty" do
+            expect(array_empty.my_all?).to eql(array_empty.all?)
+        end
+        
+        it "checking my_all with array mixed" do
+            expect(array_mixed.my_all?).to eql(array_mixed.all?)
+        end
+
+        it "checking my_all with differents kind of numbers" do
+            expect(array_numeric.my_all?(Numeric)).to eql(array_numeric.all?(Numeric))
+        end
+    end
+
+    describe '#My_any' do
+        it "checking my_any with array numbers" do
+            expect(array.my_any? { |n| n > 5 }  ).to eql(array.any? { |n| n > 5 } )
+        end
+
+        it "checking my_any with array words" do
+            expect(words.my_any? { |n| n.length > 2 }).to eql(words.any? { |n| n.length > 2 })
+        end
+
+        it "checking my_any with array empty" do
+            expect(array_empty.my_any?).to eql(array_empty.any?)
+        end
+        
+        it "checking my_any with array words" do
+            expect(array_mixed.my_any?).to eql(array_mixed.any?)
+        end
+
+        it "checking my_any with array words" do
+            expect(array_numeric.my_any?(Numeric)).to eql(array_numeric.any?(Numeric))
+        end
+    end
+
+    describe '#My_none?' do
+        it "checking my_none with array numbers" do
+            expect(array.my_count).to eql(array.count)
+        end
+
+        # it "checking my_none with array words" do
+        #     expect(words.my_none? { |n| n.length > 2 }).to eql(words.none? { |n| n.length > 2 })
+        # end
+
+        # it "checking my_none with array empty" do
+        #     expect(array_empty.my_none?).to eql(array_empty.none?)
+        # end
+        
+        # it "checking my_none with array words" do
+        #     expect(array_mixed.my_none?).to eql(array_mixed.none?)
+        # end
+
+        # it "checking my_none with array words" do
+        #     expect(array_numeric.my_none?(Numeric)).to eql(array_numeric.none?(Numeric))
+        # end
+    end
+
+    describe '#My_count?' do
+        it "checking my_none with array numbers" do
+            expect(array.my_none? { |n| n > 5 }  ).to eql(array.none? { |n| n > 5 } )
+        end
+
+        it "checking my_none with array words" do
+            expect(words.my_none? { |n| n.length > 2 }).to eql(words.none? { |n| n.length > 2 })
+        end
+
+        it "checking my_none with array empty" do
+            expect(array_empty.my_none?).to eql(array_empty.none?)
+        end
+        
+        it "checking my_none with array words" do
+            expect(array_mixed.my_none?).to eql(array_mixed.none?)
+        end
+
+        it "checking my_none with array words" do
+            expect(array_numeric.my_none?(Numeric)).to eql(array_numeric.none?(Numeric))
         end
     end
 
