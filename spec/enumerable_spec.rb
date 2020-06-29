c23f8ae2fec2ec40321a55b8d73d61a203ab4374
brugobi <brgobbi@hotmail.com>
2020-06-29 15:48:06 -0400

    refactoring my each test

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,10 +1,9 @@
 require_relative '../enumerable.rb'
 
 describe Enumerable do
-    # let(:array) { [1, 2, 3, 4, 5, 6, 7, 8, 9] }
+    let(:array) { [1, 2, 3, 4, 5, 6, 7, 8, 9] }
     describe '#My_each' do
         it "checking my_each method" do
-            array = [1,2,3,4]
             expect(array.my_each { |number| number*2 }).to eql(array.each { |number| number*2 })
         end 
     end       
